e66f895563477993d1ce53c9a37ed34b211eb5ad
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-05-06 19:58:29 -0400

    test(ota-task): make FullOtaSuccessFlow deterministc
    - Set restart_on_success to false to prevent race condition with mock restart() call.
    - Removed restart expectation.

--- host_test/test_ota_manager/main/test_ota_manager_task.cpp
+++ host_test/test_ota_manager/main/test_ota_manager_task.cpp
@@ -155,11 +155,14 @@ TEST_F(OtaManagerTaskTest, FullOtaSuccessFlow)
     uint8_t expected_sha[32] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15,
                                 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
 
+    // Modify config for deterministic state checking
+    config.restart_on_success = false;
+
     EXPECT_CALL(mock_system, get_update_partition()).WillOnce(Return(&fake_partition));
     EXPECT_CALL(mock_system, get_partition_sha256(&fake_partition, _, _))
         .WillOnce(DoAll(SetArrayArgument<2>(expected_sha, expected_sha + 32), Return(ESP_OK)));
 
-    EXPECT_CALL(mock_system, restart()).Times(1);
+    EXPECT_CALL(mock_system, restart()).Times(0);
 
     // 3. Execute
     ASSERT_TRUE(sut.init(config));
